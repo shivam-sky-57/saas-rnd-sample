b15a8540a8b3a49b4c966afc86d280852a492a1d
shivam-sky-57 <shivamkumar7965@gmail.com>
2025-08-15 19:29:15 +0530

    changed database to neon

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-17pirv_1an&hnqb%*#2=ki0w#d_wd9w_z)t61ucpha*+g1z&a8'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
+# DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+print("DEBUG", DEBUG ,type(DEBUG))
 
-print("DEBUG",DEBUG,type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app" # https://saas.prod.railway.app
@@ -92,6 +93,20 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+CONN_MAX_AGE = config("CONN_MAX_AGE",cast=int, default=30)
+DATABASE_URL = config("DATABASE_URL",cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=CONN_MAX_AGE,
+        conn_health_checks=True,
+        )
+    }
+
 
 
 # Password validation
